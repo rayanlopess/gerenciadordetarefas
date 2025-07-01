6d25cc03ab57c12d8e89a69e69a080b1b25bac26
RAYAN LOPES AMERICO <rayan.americo@alunos.sc.senac.br>
2025-07-01 14:48:28 -0300

    2.0

--- src/app/criar-tarefa/criar-tarefa.page.ts
+++ src/app/criar-tarefa/criar-tarefa.page.ts
@@ -20,22 +20,22 @@ import { Title } from '@angular/platform-browser';
   imports: [CommonModule, FormsModule, IonicModule]
 })
 export class CriarTarefaPage implements OnInit {
-  public quantidade: Array<number> = [
-    1,
-    2,
-    3,
-    4,
-    5
-  ];
+  public quantidades: Array<number> = [1, 2, 3, 4, 5];
+  public quantidade = 0;
+
   constructor(
     private titleService: Title,
-    private rt:Router
+    private rt: Router
   ) {
-    addIcons({add, trash, chevronDown, personCircle})
-   }
+    addIcons({add, trash, chevronDown, personCircle});
+  }
 
   ngOnInit() {
     this.titleService.setTitle("Adicionar Tarefa");
   }
 
+  // Método para criar um array com base na quantidade selecionada
+  getEtapasArray(): number[] {
+    return Array(this.quantidade).fill(0).map((x, i) => i);
+  }
 }
