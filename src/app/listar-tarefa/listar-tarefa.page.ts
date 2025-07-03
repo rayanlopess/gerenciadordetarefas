13641fb853a90158cb6aaa1aa505f4a230653763
Your Name <you@example.com>
2025-07-02 23:11:49 -0300

    4.0

--- src/app/listar-tarefa/listar-tarefa.page.ts
+++ src/app/listar-tarefa/listar-tarefa.page.ts
@@ -2,7 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { IonicModule } from '@ionic/angular';
-
+import { ActivatedRoute } from '@angular/router';
+import { AngularFirestore } from '@angular/fire/compat/firestore';
 
 @Component({
   selector: 'app-listar-tarefa',
@@ -11,11 +12,46 @@ import { IonicModule } from '@ionic/angular';
   standalone: true,
   imports: [IonicModule, CommonModule, FormsModule]
 })
-export class ListarTarefaPage implements OnInit {
+export class TarefaPage implements OnInit {
+  tarefaId: string = '101';
+  etapas: any[] = [];
+  progresso = 0;
+  carregando = true;
 
-  constructor() { }
+  constructor(
+    private route: ActivatedRoute,
+    private firestore: AngularFirestore
+  ) { }
 
   ngOnInit() {
+    //this.tarefaId = this.route.snapshot.paramMap.get('id');
+    this.carregarEtapas();
   }
 
+  carregarEtapas() {
+    this.firestore.collection('tarefas').doc(this.tarefaId).collection('etapas')
+      .valueChanges()
+      .subscribe((etapas: any[]) => {
+        this.etapas = etapas;
+        this.calcularProgresso();
+        this.carregando = false;
+      });
+  }
+
+  atualizarEtapa(etapa: any) {
+    this.firestore.collection('tarefas').doc(this.tarefaId)
+      .collection('etapas').doc(etapa.id).update({
+        concluida: !etapa.concluida
+      });
+  }
+
+  calcularProgresso() {
+    if (this.etapas.length === 0) {
+      this.progresso = 0;
+      return;
+    }
+    
+    const concluidas = this.etapas.filter(e => e.concluida).length;
+    this.progresso = Math.round((concluidas / this.etapas.length) * 100);
+  }
 }
